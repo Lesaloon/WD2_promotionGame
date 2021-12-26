94ef1385347e9a4820c888052c0afa23d3eaf0a2
Lesaloon <guilliancarville@gmail.com>
2021-12-26 21:02:36 +0100

    progress

--- client/app.js
+++ client/app.js
@@ -1,7 +1,25 @@
-new TypeIt('#container', {
-    strings: ["Hello", "world"],
-    speed: 50,
-    lifeLike: true,
-    startDelay: 0,
-    cursorChar: "■"
-}).go();
+const socket = new WebSocket('ws://localhost:8080');
+
+$('.terminal').terminal({
+    attack: function(server) {
+        this.echo('attacking, ' + server);
+    },
+    login: function(username) {
+        if (username.split(" ").length != 1) {
+            this.echo('Le pseudo ne peut pas avoir d espace');
+            return
+        } else {
+            socket.send("pseudo:"+username)
+        }
+    }
+}, {
+    greetings: 'Ceci est le terminal, écrivez des commandes et soyez prêt'
+});
+
+
+socket.addEventListener('open', function (event) {
+    socket.send('Hello Server!');
+});
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
